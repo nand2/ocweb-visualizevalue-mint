cc5cb696cd4cf8c5068569a07716f469150a4b22
nand <ndeschildre@gmail.com>
2024-12-08 10:56:10 +0100

    Update: Theme files

--- scripts/update.sh
+++ scripts/update.sh
@@ -25,7 +25,7 @@ if [ "$CHAIN_ID" != "31337" ] && [ -z "$ETHERSCAN_API_KEY" ]; then
 fi
 # Section.
 SECTION=${1:-}
-if [ "$SECTION" != "admin-files" ]; then
+if [ "$SECTION" != "admin-files" ] && [ "$SECTION" != "theme-files" ]; then
   echo "Invalid section: $SECTION"
   exit 1
 fi
@@ -65,11 +65,6 @@ else
   PLUGIN_ADDRESS=$(echo $PLUGINS_INFOS | awk '{for (i=2; i<=NF; i++) if ($i == "(\"visualizeValueMint\",") print $(i-1)}' | sed 's/^.\(.*\).$/\1/')
 fi
 
-# Get the frontend address
-PLUGIN_ADMIN_CALL_RESULT=$(cast call --rpc-url $RPC_URL --chain ${CHAIN_ID} $PLUGIN_ADDRESS "frontend()")
-PLUGIN_ADMIN_ADDRESS=$(cast abi-decode "frontend()(address)" $PLUGIN_ADMIN_CALL_RESULT)
-PLUGIN_ADMIN_WEB3_ADDRESS="web3://${PLUGIN_ADMIN_ADDRESS}:${CHAIN_ID}"
-
 # Update files in the admin frontend
 if [ "$SECTION" == "admin-files" ]; then
   NEW_VERSION_MESSAGE="${@:2}"
@@ -77,6 +72,11 @@ if [ "$SECTION" == "admin-files" ]; then
     echo "Please provide a message for the new version"
     exit 1
   fi
+
+  # Get the frontend address
+  PLUGIN_ADMIN_CALL_RESULT=$(cast call --rpc-url $RPC_URL --chain ${CHAIN_ID} $PLUGIN_ADDRESS "frontend()")
+  PLUGIN_ADMIN_ADDRESS=$(cast abi-decode "frontend()(address)" $PLUGIN_ADMIN_CALL_RESULT)
+  PLUGIN_ADMIN_WEB3_ADDRESS="web3://${PLUGIN_ADMIN_ADDRESS}:${CHAIN_ID}"
 
   # Go to the admin frontend folder
   cd $ROOT_FOLDER/admin
@@ -123,5 +123,96 @@ if [ "$SECTION" == "admin-files" ]; then
   echo "Setting the new website version live..."
   PRIVATE_KEY=$PRIVATE_KEY \
   WEB3_ADDRESS=${PLUGIN_ADMIN_WEB3_ADDRESS} \
+  npx ocweb --rpc $RPC_URL $OCWEB_CLI_EXTRA_ARGS version-set-live $WEBSITE_VERSION_INDEX
+fi
+
+# Update files in the theme
+if [ "$SECTION" == "theme-files" ]; then
+  THEME_NAME=${2:-}
+  if [ "$THEME_NAME" == "" ]; then
+    echo "Please provide a theme name"
+    exit 1
+  fi
+  NEW_VERSION_MESSAGE="${@:3}"
+  if [ "$NEW_VERSION_MESSAGE" == "" ]; then
+    echo "Please provide a message for the new version"
+    exit 1
+  fi
+
+  # Prepare the theme folder
+  THEME=mint-$THEME_NAME
+
+  # Prepare the human-readable name of the theme
+  # Mapping of names to human-readable names
+  case $THEME_NAME in
+    "base")
+      THEME_HUMAN_NAME="Base"
+      ;;
+    "zinc")
+      THEME_HUMAN_NAME="Zinc"
+      ;;
+    *)
+      echo "Unknown theme name: $THEME_NAME"
+      exit 1
+      ;;
+  esac
+
+  PLUGIN_THEME_ADDRESS=$(npx web3curl "web3://${PLUGIN_ADDRESS}:${CHAIN_ID}/getThemes?returns=((string,address)[])" | jq -r ".[0][] | select(.[0] == \"$THEME_HUMAN_NAME\") | .[1]")
+  if [ "$PLUGIN_THEME_ADDRESS" == "" ]; then
+    echo "Theme not found"
+    exit 1
+  fi
+  PLUGIN_THEME_WEB3_ADDRESS="web3://${PLUGIN_THEME_ADDRESS}:${CHAIN_ID}"
+
+  # Go to the theme folder
+  cd $ROOT_FOLDER/$THEME
+  # Build the theme
+  pnpm run generate
+
+  # Make a temporary folder where we prepare the upload
+  cd $ROOT_FOLDER
+  rm -Rf $ROOT_FOLDER/dist
+  mkdir -p dist
+  cp -R $THEME/.output/public/* dist
+  # We inject the contents of assets/mint-index-patch.html into the base index.html, just before the </body> tag
+  node -e "
+    const fs = require('fs');
+    const patch = fs.readFileSync('$ROOT_FOLDER/assets/mint-index-patch.html', 'utf8');
+    fs.writeFileSync('$ROOT_FOLDER/dist/index.html', fs.readFileSync('$ROOT_FOLDER/dist/index.html', 'utf8').replace(/<\/body>/i, patch + '\n</body>'));
+  "
+
+  echo "Updating files on theme ${PLUGIN_THEME_WEB3_ADDRESS} ..."
+
+  # First create a new website version
+  echo "Creating a new website version..."
+  PRIVATE_KEY=$PRIVATE_KEY \
+  WEB3_ADDRESS=${PLUGIN_THEME_WEB3_ADDRESS} \
+  npx ocweb --rpc $RPC_URL $OCWEB_CLI_EXTRA_ARGS version-add "$NEW_VERSION_MESSAGE"
+
+  sleep 1
+
+  # Get the number of the newly created version
+  echo "Fetching the number of the new website version..."
+  WEBSITE_VERSION_INDEX=$(WEB3_ADDRESS=${PLUGIN_THEME_WEB3_ADDRESS} \
+  npx ocweb --rpc $RPC_URL $OCWEB_CLI_EXTRA_ARGS version-ls | tail -n1 | awk '{print $1}')
+  echo "New website version number: $WEBSITE_VERSION_INDEX"
+
+  # Make it viewable
+  echo "Making the new website version viewable..."
+  PRIVATE_KEY=$PRIVATE_KEY \
+  WEB3_ADDRESS=${PLUGIN_THEME_WEB3_ADDRESS} \
+  npx ocweb --rpc $RPC_URL $OCWEB_CLI_EXTRA_ARGS --website-version $WEBSITE_VERSION_INDEX version-set-viewable true
+
+  # Upload the files to the new website version
+  echo "Uploading files to the new website version..."
+  PRIVATE_KEY=$PRIVATE_KEY \
+  WEB3_ADDRESS=${PLUGIN_THEME_WEB3_ADDRESS} \
+  npx ocweb --rpc $RPC_URL $OCWEB_CLI_EXTRA_ARGS --website-version $WEBSITE_VERSION_INDEX \
+  upload dist/* / --sync
+
+  # Set the new website version live
+  echo "Setting the new website version live..."
+  PRIVATE_KEY=$PRIVATE_KEY \
+  WEB3_ADDRESS=${PLUGIN_THEME_WEB3_ADDRESS} \
   npx ocweb --rpc $RPC_URL $OCWEB_CLI_EXTRA_ARGS version-set-live $WEBSITE_VERSION_INDEX
 fi
